61502e83efbb01d54c43530cd1b25d0aabc8ab96
egabskr <egabnsoswet@gmail.com>
2026-01-27 15:05:21 +0700

    Script Image Storage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { clearOldImageStorages } from './image-storage';
 
 window.Alpine = Alpine;
+window.addEventListener('DOMContentLoaded', function() {
+    clearOldImageStorages();
+});
+
 
 Alpine.start();
