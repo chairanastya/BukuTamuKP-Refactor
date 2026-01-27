3e584aa393ff86fd3b5f0310a77ddc585e3d67ac
Kayana Azaria <kayana.azaria@gmail.com>
2026-01-27 11:51:24 +0700

    import DataTableManager to app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { DataTableManager } from './datatables-init.js';
 
 window.Alpine = Alpine;
+window.DataTableManager = DataTableManager;
 
 Alpine.start();
