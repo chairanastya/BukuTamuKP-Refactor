f26f21bff65ddc303b36e9d41f0af6b3ffdf15ba
chairanastya <chairanastya@gmail.com>
2026-01-27 12:16:40 +0700

    fix pdf export script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { exportDataTablePDF, exportContentPDF } from './pdf-export.js';
 
 window.Alpine = Alpine;
+window.exportDataTablePDF = exportDataTablePDF;
+window.exportContentPDF = exportContentPDF;
 
 Alpine.start();
