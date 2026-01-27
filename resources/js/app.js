be1f2f5381c52a8584c09cf21a682da8b60e02ae
Kayana Azaria <kayana.azaria@gmail.com>
2026-01-27 12:05:37 +0700

    add input background update script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { updateInputBackground, initInputBackgrounds } from './input-background.js';
 
 window.Alpine = Alpine;
+window.updateInputBackground = updateInputBackground;
+window.initInputBackgrounds = initInputBackgrounds;
 
 Alpine.start();
