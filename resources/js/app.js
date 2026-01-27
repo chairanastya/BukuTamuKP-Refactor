22d5861f6d84836da9c64d9c67a141e3a56672cf
Kayana Azaria <kayana.azaria@gmail.com>
2026-01-27 14:41:45 +0700

    add reCAPTCHA script

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+// Import reCAPTCHA utilities and make them globally available
+import * as Recaptcha from './captcha';
+
 window.Alpine = Alpine;
+window.Recaptcha = Recaptcha;
 
 Alpine.start();
