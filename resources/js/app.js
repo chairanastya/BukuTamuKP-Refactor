ad037fa562cf2489d1a2fa79cad5ba87517515ed
Kayana Azaria <kayana.azaria@gmail.com>
2026-01-27 13:54:56 +0700

    add karyawan row manager script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,20 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import * as KaryawanRowManager from './karyawan-row-manager.js';
 
 window.Alpine = Alpine;
+window.addKaryawanRow = KaryawanRowManager.addKaryawanRow;
+window.removeKaryawanRow = KaryawanRowManager.removeKaryawanRow;
+window.setupRowListeners = KaryawanRowManager.setupRowListeners;
+window.searchKaryawan = KaryawanRowManager.searchKaryawan;
+window.displayAutocomplete = KaryawanRowManager.displayAutocomplete;
+window.selectKaryawan = KaryawanRowManager.selectKaryawan;
+window.renderKaryawanCard = KaryawanRowManager.renderKaryawanCard;
+window.updateHiddenInput = KaryawanRowManager.updateHiddenInput;
+window.resetKaryawanRow = KaryawanRowManager.resetKaryawanRow;
+window.updateMinusButtonsVisibility = KaryawanRowManager.updateMinusButtonsVisibility;
+window.setSearchKaryawanRoute = KaryawanRowManager.setSearchKaryawanRoute;
+window.setEscapeHtmlFn = KaryawanRowManager.setEscapeHtmlFn;
 
 Alpine.start();
