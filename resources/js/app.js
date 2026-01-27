3cc853dff7161a103bd31e647587e3f55777a3c9
egabskr <egabnsoswet@gmail.com>
2026-01-27 15:26:20 +0700

    Ubah app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createAutocomplete } from './autocomplete';
 
 window.Alpine = Alpine;
+window.createAutocomplete = createAutocomplete;
 
 Alpine.start();
