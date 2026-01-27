133b15b8a817dddf65f85101ba4ee2d031547b9b
egabskr <egabnsoswet@gmail.com>
2026-01-27 14:25:44 +0700

    Tambah app.js loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ import Alpine from 'alpinejs';
 import { initLoadingSpinner } from './loading-spinner';
 
 window.Alpine = Alpine;
+window.showLoading = showLoading;
+window.hideLoading = hideLoading;
+window.createInlineSpinner = createInlineSpinner;
 
 Alpine.start();
 
