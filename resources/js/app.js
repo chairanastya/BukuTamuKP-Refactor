6af4d23220ece7a49e41623c76cb0bd15873db1c
egabskr <egabnsoswet@gmail.com>
2026-01-27 14:03:43 +0700

    Revisi app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,13 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import { setupFormValidation, validateNama, validateEmail } from './form-validation';
+
 window.Alpine = Alpine;
+
+// Expose form validation helpers to global scope so blade views can call them
+window.setupFormValidation = setupFormValidation;
+window.validateNama = validateNama;
+window.validateEmail = validateEmail;
 
 Alpine.start();
