e6037efde588f183adc64746daaac29840790ed9
Kayana Azaria <kayana.azaria@gmail.com>
2026-01-27 11:43:32 +0700

    add filter by status script

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import { createStatusFilter } from './status-filter.js';
 
 window.Alpine = Alpine;
+window.createStatusFilter = createStatusFilter;
 
 Alpine.start();
