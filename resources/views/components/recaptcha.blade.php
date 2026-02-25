560614ad80a06c087f268b837465ef1bc6828efe
chairanastya <chairanastya@gmail.com>
2026-02-25 11:09:53 +0700

    Add reCAPTCHA callbacks and debug logging

--- resources/views/components/recaptcha.blade.php
+++ resources/views/components/recaptcha.blade.php
@@ -7,12 +7,34 @@
     <div class="g-recaptcha" 
          data-sitekey="{{ config('services.recaptcha.site_key') }}"
          data-theme="{{ $theme }}"
-         data-size="{{ $size }}">
+         data-size="{{ $size }}"
+         data-callback="onRecaptchaSuccess"
+         data-error-callback="onRecaptchaError">
     </div>
 </div>
 
 @once
     @push('head')
+        <script>
+            function onRecaptchaSuccess(token) {
+                console.log('[reCAPTCHA] Token received:', token ? token.substring(0, 20) + '...' : 'empty');
+                // Token is automatically placed in hidden field by Google
+            }
+            
+            function onRecaptchaError() {
+                console.error('[reCAPTCHA] Error callback triggered');
+            }
+
+            window.addEventListener('DOMContentLoaded', function() {
+                console.log('[reCAPTCHA] DOM Ready - script should initialize');
+                // Check if grecaptcha is available
+                if (typeof grecaptcha !== 'undefined') {
+                    console.log('[reCAPTCHA] grecaptcha is ready');
+                } else {
+                    console.warn('[reCAPTCHA] grecaptcha not yet available');
+                }
+            });
+        </script>
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     @endpush
 @endonce
