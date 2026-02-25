61161a4ba7698b39d17e3490bb057a36b4c0da43
chairanastya <chairanastya@gmail.com>
2026-02-25 11:09:13 +0700

    Simplify reCAPTCHA - use standard Google script without callbacks

--- resources/views/components/recaptcha.blade.php
+++ resources/views/components/recaptcha.blade.php
@@ -13,31 +13,6 @@
 
 @once
     @push('head')
-        <script>
-            // Prevent Google script from running before DOM is ready
-            window.__RECAPTCHA_DEFER__ = true;
-        </script>
-        <script src="https://www.google.com/recaptcha/api.js?onload=onRecaptchaLoad&render=implicit" async defer></script>
-        <script>
-            // Callback when Google API is ready
-            window.onRecaptchaLoad = function() {
-                console.log('[reCAPTCHA] Google API loaded');
-                // Defer rendering to next tick
-                setTimeout(function() {
-                    if (typeof grecaptcha !== 'undefined') {
-                        console.log('[reCAPTCHA] Ready to use');
-                    }
-                }, 100);
-            };
-            
-            // Ensure div is rendered before Google script runs
-            if (document.readyState === 'loading') {
-                document.addEventListener('DOMContentLoaded', function() {
-                    window.__RECAPTCHA_DEFER__ = false;
-                });
-            } else {
-                window.__RECAPTCHA_DEFER__ = false;
-            }
-        </script>
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     @endpush
 @endonce
