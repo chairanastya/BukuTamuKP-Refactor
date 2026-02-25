335d04d69c68269a51c878fe8d8cd542f060dc21
chairanastya <chairanastya@gmail.com>
2026-02-25 11:05:10 +0700

    Revert to implicit reCAPTCHA rendering - simpler and more reliable

--- resources/views/components/recaptcha.blade.php
+++ resources/views/components/recaptcha.blade.php
@@ -5,48 +5,14 @@
 
 <div {{ $attributes->merge(['class' => 'flex justify-center']) }}>
     <div class="g-recaptcha" 
-         id="recaptcha-container"
          data-sitekey="{{ config('services.recaptcha.site_key') }}"
          data-theme="{{ $theme }}"
-         data-size="{{ $size }}"
-         data-callback="onRecaptchaSuccess">
+         data-size="{{ $size }}">
     </div>
 </div>
 
-@push('head')
-    <script src="https://www.google.com/recaptcha/api.js?onload=onRecaptchaLoad&render=explicit" async defer></script>
-@endpush
-
-@push('scripts')
-    <script>
-        // Global callback for reCAPTCHA load
-        window.onRecaptchaLoad = function() {
-            console.log('reCAPTCHA API loaded');
-            // Force render if container exists
-            var container = document.getElementById('recaptcha-container');
-            if (container && typeof grecaptcha !== 'undefined') {
-                try {
-                    grecaptcha.render('recaptcha-container', {
-                        'sitekey': '{{ config('services.recaptcha.site_key') }}',
-                        'theme': '{{ $theme }}',
-                        'size': '{{ $size }}'
-                    });
-                    console.log('reCAPTCHA rendered successfully');
-                } catch (e) {
-                    console.error('Error rendering reCAPTCHA:', e);
-                }
-            }
-        };
-
-        // Callback when user completes captcha
-        window.onRecaptchaSuccess = function(token) {
-            console.log('reCAPTCHA token received:', token.substring(0, 20) + '...');
-        };
-
-        // Ensure grecaptcha is ready before using
-        window.grecaptchaReady = function() {
-            console.log('grecaptcha is ready');
-            return typeof grecaptcha !== 'undefined';
-        };
-    </script>
-@endpush
+@once
+    @push('head')
+        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    @endpush
+@endonce
